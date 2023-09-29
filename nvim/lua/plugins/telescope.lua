309d0b48ab04101c6af33f047e8601e8b4b8006e
p4luch <luk.pauszek@gmail.com>
2023-09-29 10:04:59 +0200

    Update packages and add .tmux.conf

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,15 +4,15 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "debugloop/telescope-undo.nvim"
+      "debugloop/telescope-undo.nvim",
     },
     keys = {
-      {"<Leader>ff", "<cmd>Telescope find_files find_command=rg,--files,--hidden,-g,!.git<cr>", opts},
-      {"<Leader>fg", "<cmd>Telescope live_grep<cr>", opts},
-      {"<Leader>fb", "<cmd>Telescope buffers<cr>", opts},
-      {"<Leader>fh", "<cmd>Telescope help_tags<cr>", opts},
-      {"<Leader>bf", "<cmd>Telescope file_browser<cr>", opts},
-      {"<Leader>u", "<cmd>Telescope undo<cr>", opts}
+      { "<Leader>ff", "<cmd>Telescope find_files find_command=rg,--files,--hidden,-g,!.git<cr>", opts },
+      { "<Leader>fg", "<cmd>Telescope live_grep<cr>", opts },
+      { "<Leader>fb", "<cmd>Telescope buffers<cr>", opts },
+      { "<Leader>fh", "<cmd>Telescope help_tags<cr>", opts },
+      { "<Leader>bf", "<cmd>Telescope file_browser<cr>", opts },
+      { "<Leader>u", "<cmd>Telescope undo<cr>", opts },
     },
     config = function()
       require("telescope").setup({
@@ -21,7 +21,7 @@ return {
             fuzzy = true,
             override_generic_sorter = true,
             override_file_sorter = true,
-            case_mode = "smart_case"
+            case_mode = "smart_case",
           },
           file_browser = {
             hijack_netrw = true,
@@ -31,13 +31,13 @@ return {
             layout_strategy = "vertical",
             layout_config = {
               preview_height = 0.8,
-            }
-          }
-        }
+            },
+          },
+        },
       })
       require("telescope").load_extension("fzf")
       require("telescope").load_extension("file_browser")
       require("telescope").load_extension("undo")
     end,
-  }
+  },
 }
