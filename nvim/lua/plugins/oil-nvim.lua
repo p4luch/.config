bd4b9092dec79def613f73131407ad314cebfc71
p4luch <luk.pauszek@gmail.com>
2025-01-24 22:37:43 +0100

    Add oil nvim

---
+++ nvim/lua/plugins/oil-nvim.lua
@@ -0,0 +1,17 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+  keys = {
+    { "<Leader>bf", "<CMD>Oil --float<CR>", desc = "Open oil in cwd", { silent = true, noremap = true } },
+  },
+}
